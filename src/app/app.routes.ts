8e62c1cd5f9232b9ac5de08071b4a6d049d5ee28
Hoomam <hoaz1600@student.miun.se>
2024-06-01 20:25:25 +0200

    site title and checkout styling

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -4,8 +4,8 @@ import { OrderComponent } from './order/order.component';
 
 
 export const routes: Routes = [
-    {path:"home", component: HomeComponent},
-    {path:"order", component: OrderComponent},
+    {path:"home", component: HomeComponent, title: "Fetch-Burger"},
+    {path:"order", component: OrderComponent, title: "Fetch-Burger"},
     {path: "", redirectTo: "home", pathMatch: "full"},
     {path: "**", component: HomeComponent}
 ];
